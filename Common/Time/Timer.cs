31bdfe74f2fb87717def6210b44bfd7ceaf4b252
Ali Salehi <nysalehi@gmail.com>
2025-04-06 23:06:26 +0200

    Add a timer class.

---
+++ Common/Time/Timer.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+
+namespace Tyr.Common.Time;
+
+public class Timer
+{
+    private readonly Stopwatch _stopwatch = new();
+
+    private long _timeOffsetTicks = 0;
+    private long _lastUpdateTicks = 0;
+
+    private const float SmoothnessAlpha = 0.1f;
+
+    public float DeltaTime { get; private set; }
+    public float DeltaTimeSmooth { get; private set; }
+
+    public bool Running => _stopwatch.IsRunning;
+
+    public float Time => (float)((_stopwatch.ElapsedTicks + _timeOffsetTicks) / (double)Stopwatch.Frequency);
+
+    public void SetTime(float value)
+    {
+        var targetTicks = (long)(value * Stopwatch.Frequency);
+        var currentTicks = _stopwatch.ElapsedTicks;
+
+        _timeOffsetTicks = targetTicks - currentTicks;
+    }
+
+    public void Reset()
+    {
+        _stopwatch.Restart();
+        _lastUpdateTicks = _stopwatch.ElapsedTicks;
+        _timeOffsetTicks = 0;
+    }
+
+    public void Restart()
+    {
+        Reset();
+        Start();
+    }
+
+    public void Stop()
+    {
+        _stopwatch.Stop();
+    }
+
+    public void Start()
+    {
+        _stopwatch.Start();
+        _lastUpdateTicks = _stopwatch.ElapsedTicks;
+    }
+
+    public void Update()
+    {
+        var nowTicks = _stopwatch.ElapsedTicks;
+        var deltaTicks = nowTicks - _lastUpdateTicks;
+
+        DeltaTime = (float)(deltaTicks / (double)Stopwatch.Frequency);
+        DeltaTimeSmooth = SmoothnessAlpha * DeltaTime + (1f - SmoothnessAlpha) * DeltaTimeSmooth;
+
+        _lastUpdateTicks = _stopwatch.ElapsedTicks;
+    }
+}
