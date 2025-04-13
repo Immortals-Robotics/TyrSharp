b6b77969160373e9e0a141fb5d04aa0901e3482c
Ali Salehi <nysalehi@gmail.com>
2025-04-13 16:22:46 +0200

    Convert config names to snake case.

--- Common/Config/New/ConfigSerialization.cs
+++ Common/Config/New/ConfigSerialization.cs
@@ -1,28 +1,34 @@
-﻿namespace Tyr.Common.Config.New;
+﻿using Tomlyn.Helpers;
+
+namespace Tyr.Common.Config.New;
 
 public class ConfigSerialization
 {
-    public static Dictionary<string, object> ToTree(IEnumerable<Configurable> configurables)
+    public static readonly Func<string, string> ConvertName = TomlNamingHelper.PascalToSnakeCase;
+
+    // Converts a list of Configurable into a nested tree
+    public static IDictionary<string, object> ToTree(IEnumerable<Configurable> configurables)
     {
         var root = new Dictionary<string, object>();
 
         foreach (var configurable in configurables)
         {
-            var nParts = configurable.Namespace.Split('.');
+            var namespaceParts = configurable.Namespace.Split('.').Skip(1);
             var current = root;
 
-            foreach (var part in nParts)
+            foreach (var namespacePart in namespaceParts)
             {
-                if (!current.TryGetValue(part, out var child))
-                    current[part] = child = new Dictionary<string, object>();
+                var convertedName = ConvertName(namespacePart);
+                if (!current.TryGetValue(convertedName, out var child))
+                    current[convertedName] = child = new Dictionary<string, object>();
                 current = (Dictionary<string, object>)child!;
             }
 
             var entries = configurable
                 .Entries()
-                .ToDictionary(entry => entry.Name, entry => entry.Value);
+                .ToDictionary(entry => ConvertName(entry.Name), entry => entry.Value);
 
-            current[configurable.TypeName] = entries;
+            current[ConvertName(configurable.TypeName)] = entries;
         }
 
         return root;
