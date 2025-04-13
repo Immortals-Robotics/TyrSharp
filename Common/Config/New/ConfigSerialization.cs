3658a38f4b1164b900b3236254b9b8e2444e04f0
Ali Salehi <nysalehi@gmail.com>
2025-04-13 16:06:09 +0200

    Add config serialization.

---
+++ Common/Config/New/ConfigSerialization.cs
@@ -0,0 +1,30 @@
+﻿namespace Tyr.Common.Config.New;
+
+public class ConfigSerialization
+{
+    public static Dictionary<string, object> ToTree(IEnumerable<Configurable> configurables)
+    {
+        var root = new Dictionary<string, object>();
+
+        foreach (var configurable in configurables)
+        {
+            var nParts = configurable.Namespace.Split('.');
+            var current = root;
+
+            foreach (var part in nParts)
+            {
+                if (!current.TryGetValue(part, out var child))
+                    current[part] = child = new Dictionary<string, object>();
+                current = (Dictionary<string, object>)child!;
+            }
+
+            var entries = configurable
+                .Entries()
+                .ToDictionary(entry => entry.Name, entry => entry.Value);
+
+            current[configurable.TypeName] = entries;
+        }
+
+        return root;
+    }
+}
