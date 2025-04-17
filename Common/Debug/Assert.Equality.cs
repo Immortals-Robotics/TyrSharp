e6a878c8c4d6465ead5454921931bae908dfa601
Ali Salehi <nysalehi@gmail.com>
2025-04-17 16:26:00 +0200

    Add more assertions and break it down into multiple files.

---
+++ Common/Debug/Assert.Equality.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Tyr.Common.Debug;
+
+public partial class Assert
+{
+    public void AreEqual<T>(T expected, T actual,
+        [CallerArgumentExpression("expected")] string? expectedExpr = null,
+        [CallerArgumentExpression("actual")] string? actualExpr = null,
+        [CallerMemberName] string? member = null,
+        [CallerFilePath] string? file = null,
+        [CallerLineNumber] int line = 0)
+    {
+        if (!Enabled || EqualityComparer<T>.Default.Equals(expected, actual)) return;
+        logger.ZLogError($"AreEqual failed: {expectedExpr} ({expected}) != {actualExpr} ({actual})", null,
+            member, file, line);
+    }
+
+    public void AreNotEqual<T>(T notExpected, T actual,
+        [CallerArgumentExpression("notExpected")]
+        string? notExpectedExpr = null,
+        [CallerArgumentExpression("actual")] string? actualExpr = null,
+        [CallerMemberName] string? member = null,
+        [CallerFilePath] string? file = null,
+        [CallerLineNumber] int line = 0)
+    {
+        if (!Enabled || !EqualityComparer<T>.Default.Equals(notExpected, actual)) return;
+        logger.ZLogError($"AreNotEqual failed: {notExpectedExpr} ({notExpected}) == {actualExpr} ({actual})", null,
+            member, file, line);
+    }
+}
---
+++ Common/Debug/Assert.Numeric.cs
@@ -0,0 +1,103 @@
+﻿using System.Runtime.CompilerServices;
+using Tyr.Common.Math;
+
+namespace Tyr.Common.Debug;
+
+public partial class Assert
+{
+    public void AreApproximatelyEqual(float expected, float actual, float tolerance = 0.001f,
+        [CallerArgumentExpression("expected")] string? expectedExpr = null,
+        [CallerArgumentExpression("actual")] string? actualExpr = null,
+        [CallerMemberName] string? member = null,
+        [CallerFilePath] string? file = null,
+        [CallerLineNumber] int line = 0)
+    {
+        if (!Enabled || Utils.ApproximatelyEqual(expected, actual, tolerance)) return;
+        logger.ZLogError(
+            $"AreApproximatelyEqual failed: {expectedExpr} ({expected}) ≠ {actualExpr} ({actual}) | tolerance: {tolerance}",
+            null, member, file, line);
+    }
+
+    public void AreNotApproximatelyEqual(float expected, float actual, float tolerance = 0.001f,
+        [CallerArgumentExpression("expected")] string? expectedExpr = null,
+        [CallerArgumentExpression("actual")] string? actualExpr = null,
+        [CallerMemberName] string? member = null,
+        [CallerFilePath] string? file = null,
+        [CallerLineNumber] int line = 0)
+    {
+        if (!Enabled || !Utils.ApproximatelyEqual(expected, actual, tolerance)) return;
+        logger.ZLogError(
+            $"AreNotApproximatelyEqual failed: {expectedExpr} ({expected}) ≈ {actualExpr} ({actual}) | tolerance: {tolerance}",
+            null, member, file, line);
+    }
+
+    public void IsZero(float value,
+        [CallerArgumentExpression("value")] string? expr = null,
+        [CallerMemberName] string? member = null,
+        [CallerFilePath] string? file = null,
+        [CallerLineNumber] int line = 0)
+    {
+        if (!Enabled || value == 0) return;
+        logger.ZLogError($"Expected zero: {expr} ({value})", null, member, file, line);
+    }
+
+    public void IsApproximatelyZero(float value, float tolerance = 0.001f,
+        [CallerArgumentExpression("value")] string? expr = null,
+        [CallerMemberName] string? member = null,
+        [CallerFilePath] string? file = null,
+        [CallerLineNumber] int line = 0)
+    {
+        if (!Enabled || Utils.ApproximatelyEqual(value, 0f, tolerance)) return;
+        logger.ZLogError($"Expected ~zero: {expr} ({value})  | tolerance: {tolerance}", null, member, file, line);
+    }
+
+    public void IsPositive(float value,
+        [CallerArgumentExpression("value")] string? expr = null,
+        [CallerMemberName] string? member = null,
+        [CallerFilePath] string? file = null,
+        [CallerLineNumber] int line = 0)
+    {
+        if (!Enabled || value > 0) return;
+        logger.ZLogError($"Expected positive value: {expr} ({value})", null, member, file, line);
+    }
+
+    public void IsNegative(float value,
+        [CallerArgumentExpression("value")] string? expr = null,
+        [CallerMemberName] string? member = null,
+        [CallerFilePath] string? file = null,
+        [CallerLineNumber] int line = 0)
+    {
+        if (!Enabled || value < 0) return;
+        logger.ZLogError($"Expected negative value: {expr} ({value})", null, member, file, line);
+    }
+
+    public void IsFinite(float value,
+        [CallerArgumentExpression("value")] string? expr = null,
+        [CallerMemberName] string? member = null,
+        [CallerFilePath] string? file = null,
+        [CallerLineNumber] int line = 0)
+    {
+        if (!Enabled || !float.IsInfinity(value) && !float.IsNaN(value)) return;
+        logger.ZLogError($"Expected finite number: {expr} ({value})", null, member, file, line);
+    }
+
+    public void IsNotNaN(float value,
+        [CallerArgumentExpression("value")] string? expr = null,
+        [CallerMemberName] string? member = null,
+        [CallerFilePath] string? file = null,
+        [CallerLineNumber] int line = 0)
+    {
+        if (!Enabled || !float.IsNaN(value)) return;
+        logger.ZLogError($"Expected non-NaN value: {expr}", null, member, file, line);
+    }
+
+    public void InRange(float value, float min, float max,
+        [CallerArgumentExpression("value")] string? expr = null,
+        [CallerMemberName] string? member = null,
+        [CallerFilePath] string? file = null,
+        [CallerLineNumber] int line = 0)
+    {
+        if (!Enabled || (value >= min && value <= max)) return;
+        logger.ZLogError($"Expected {expr} to be in range [{min}, {max}], but was {value}", null, member, file, line);
+    }
+}
