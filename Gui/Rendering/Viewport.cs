e9d58866259362a67a61ff838313fdf9f01565db
Ali Salehi <nysalehi@gmail.com>
2025-04-30 03:25:34 +0200

    Make camera viewport a struct.

--- Gui/Rendering/Viewport.cs
+++ Gui/Rendering/Viewport.cs
@@ -2,7 +2,7 @@
 
 namespace Tyr.Gui.Rendering;
 
-public record Viewport(
+public readonly record struct Viewport(
     Vector2 Offset, // Top-left in screen space (e.g. from GetCursorScreenPos)
     Vector2 Size // Size of the region in pixels
 );
