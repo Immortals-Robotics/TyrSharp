efbb6d4e2f59ed36be6d4a98a6a0ccb3874b0070
Ali Salehi <nysalehi@gmail.com>
2026-04-14 23:19:02 +0200

    Make `Any` properties nullable and fix formatting

--- Common/Data/Any.cs
+++ Common/Data/Any.cs
@@ -3,7 +3,8 @@
 namespace Tyr.Common.Data;
 
 [ProtoContract]
-public sealed class Any {
-    [ProtoMember(1)] public string TypeUrl {get;set;}
-    [ProtoMember(2)] public byte[] Value {get;set;}
+public sealed class Any
+{
+    [ProtoMember(1)] public string? TypeUrl { get; set; }
+    [ProtoMember(2)] public byte[]? Value { get; set; }
 }
