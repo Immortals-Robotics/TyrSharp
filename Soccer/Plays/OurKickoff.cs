72e144605b4b3103b3c5d7151c8a5253ea46e89c
Mohammad Shirazi <mhmmdshirazi@gmail.com>
2026-04-17 21:27:33 +0200

    kickoff

--- Soccer/Plays/OurKickoff.cs
+++ Soccer/Plays/OurKickoff.cs
@@ -1,4 +1,6 @@
-﻿using Tyr.Soccer.Role;
+﻿using System.Numerics;
+using Tyr.Common.Math;
+using Tyr.Soccer.Role;
 
 namespace Tyr.Soccer.Plays;
 
@@ -8,15 +10,45 @@ public class OurKickoff : IPlay
 
     public Formation Tick()
     {
+        var ballPos = Context.Ball.State.Position;
+        var side = -Context.SideSign; // Side we are attacking towards
+
+        // Receiver positions based on C++ kickoffUsPass
+        var mid5Pos = new Vector2(ballPos.X + side * 150f, Context.Field.Height - 300f);
+        var mid1Pos = new Vector2(ballPos.X + side * 150f, -(Context.Field.Height - 300f));
+        var mid2Pos = ballPos.PointOnConnectingLine(Context.Field.OwnGoal(), 1000f);
+
+        var elapsed = Context.Referee.Elapsed(Context.Time);
+        var canKick = Context.Referee.CanKickBall();
+        // The attacker targets mid5 (the top receiver)
+        var attackerTarget = mid5Pos;
+        var shootPower = 0f;
+
+
+        shootPower = canKick ? 3000f : 0f;
+
+
+        Log.ZLogDebug($"elapsed: {elapsed}");
+
         return new Formation
         {
             RequiredRoles =
             [
+                new Goalie(),
+                new Defender(1),
+                new Defender(2),
                 new CircleBall()
                 {
-                    TargetPosition = Context.Field.OppGoal(),
-                    CanKick = Context.Referee.CanKickBall(),
+                    TargetPosition = attackerTarget,
+                    CanKick = canKick,
+                    ShootPower = shootPower
                 },
+            ],
+            DesiredRoles =
+            [
+                new Waiter(mid5Pos),
+                new Waiter(mid1Pos),
+                new Waiter(mid2Pos),
             ]
         };
     }
