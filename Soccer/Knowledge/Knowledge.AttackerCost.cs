0c6b00486c674cce4a4c27a075c6ae62bd564e04
Ali Salehi <nysalehi@gmail.com>
2026-04-19 11:49:04 +0200

    Take physical ability into account for attacker role cost.

--- Soccer/Knowledge/Knowledge.AttackerCost.cs
+++ Soccer/Knowledge/Knowledge.AttackerCost.cs
@@ -1,6 +1,5 @@
 using Tyr.Common;
 using Tyr.Common.Config;
-using Tyr.Common.Extensions;
 using Tyr.Common.Time;
 using Tyr.Soccer.Navigation.Trajectory;
 using Tyr.Soccer.Robot;
@@ -13,21 +12,21 @@ public partial class Knowledge
     [ConfigEntry("Ball-speed threshold for switching attacker assignment from static-ball reach to moving-ball interception [mm/s]")]
     private static float AttackerStaticBallMaxSpeedMmPerS { get; set; } = 300f;
 
+    [ConfigEntry] private static DeltaTime AttackerAssignmentNoKickPenalty { get; set; } = DeltaTime.FromSeconds(5f);
+    [ConfigEntry] private static DeltaTime AttackerAssignmentNoDribblerPenalty { get; set; } = DeltaTime.FromSeconds(0.5f);
+
+
     private readonly Dictionary<int, DeltaTime> _attackerAssignmentCosts = [];
 
     public DeltaTime GetAttackerAssignmentCost(RobotRef robot)
     {
-        if (IsOurGoalie(robot))
-        {
-            return DeltaTime.MaxValue;
-        }
-
         if (_attackerAssignmentCosts.TryGetValue(robot.Id, out var cached))
         {
             return cached;
         }
 
-        return CalculateAttackerAssignmentCost(robot);
+        return CalculateAttackerAssignmentCost(robot) +
+               CalculateAttackerAbilityPenalty(robot);
     }
 
     private void UpdateAttackerAssignmentCosts()
@@ -36,18 +35,30 @@ public partial class Knowledge
 
         foreach (var robot in Context.OwnRobots)
         {
-            if (!robot.Seen || IsOurGoalie(robot))
+            if (!robot.Seen)
             {
                 continue;
             }
 
-            _attackerAssignmentCosts[robot.Id] = CalculateAttackerAssignmentCost(robot);
+            _attackerAssignmentCosts[robot.Id] = CalculateAttackerAssignmentCost(robot) +
+                                                 CalculateAttackerAbilityPenalty(robot);
         }
     }
 
-    private static bool IsOurGoalie(RobotRef robot)
+    private DeltaTime CalculateAttackerAbilityPenalty(RobotRef robot)
     {
-        return robot.Id == (int)Context.Referee.OurInfo().Goalkeeper;
+        var penalty = DeltaTime.Zero;
+        if (!robot.PhysicalStatus.HasDirectKick || !robot.PhysicalStatus.HasChipKick)
+        {
+            penalty += AttackerAssignmentNoKickPenalty;
+        }
+
+        if (!robot.PhysicalStatus.HasDribbler)
+        {
+            penalty += AttackerAssignmentNoDribblerPenalty;
+        }
+
+        return penalty;
     }
 
     private DeltaTime CalculateAttackerAssignmentCost(RobotRef robot)
